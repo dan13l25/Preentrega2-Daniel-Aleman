5114bd1b37f0b734b07bbae12d70193661d87f7f
Daniel Alemán <dan13laleman14@gmail>
2023-12-16 01:13:24 -0300

    ItemList con firebase

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,13 @@ import {Link} from "react-router-dom";
 const NavBar = () => {
     return (
         <nav>
-            <div>
+            <div className="navegacion">
                 <Link to="/">Home</Link>
                 <Link to="galeria">Galeria</Link>
                 <Link to="productos">Productos</Link>
+            </div>
+            <div className="ingreso">
+                
             </div>
         </nav>
     )
--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,4 +1,4 @@
-import listaProductos from "../components/data";
+/*import listaProductos from "../components/data";
 import "./Productos.css"
 import { Link } from "react-router-dom";
 
@@ -22,4 +22,44 @@ function Productos(){
     )
 }
 
-export default Productos;
+export default Productos;*/
+
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Productos.css"
+
+function Productos(){
+    const itemCollectionRef = collection(db,"producto")
+    const [itemList, setItemList] = useState([])
+    
+    useEffect(()=>{
+        const getItemList = async ()=>{
+            const data = await getDocs(itemCollectionRef)
+            const filteredData = data.docs.map( (doc)=>({
+                ...doc.data(),
+                id:doc.id
+            })) 
+            setItemList(filteredData)
+        }
+        getItemList()
+    },[])
+
+    return (
+        <div className="producto">
+            {itemList.map((item)=>(
+                <div key={item.id}>
+                    <h2>{item.title}</h2>
+                    <img src= {item.image} alt={item.title} />
+                    <Link to={`/productos/${item.id}`}>Precio y detalle</Link>
+                </div>
+                
+            ))
+
+            }
+        </div>
+    )
+}
+
+export default Productos
